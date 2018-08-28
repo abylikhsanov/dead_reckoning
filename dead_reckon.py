15a71d19f9019e68f5329943906703785be45ae1
Abyl Ikhsanov <abyl.ikhsanov@gmail.com>
2018-08-28 15:41:17 +0600

    three poss

--- dead_reckon.py
+++ dead_reckon.py
@@ -11,16 +11,26 @@ def dead_reckon(T,w_l, w_r,l,r,theta, posx, posy):
 	posx_T = 0.0
 	posy_T = 0.0
 	s = 0.0
+	vel_and_ang = calc_lin_vel_and_ang(l,w_l,w_r,r)
 	if w_l == w_r:
-		v = calc_lin_vel_and_theta(l,w_l,w_r,r)[0]
+		v = vel_and_ang[0][0]
 		s = v*T
 		theta_T = theta
 		posx_T = posx + (s*np.cos(theta_T))
 		posy_T = posy + (s*np.sin(theta_T))
 
 	elif abs(w_l) == abs(w_r):
-		angular = calc_lin_vel_and_theta(l,w_l,w_r,r)[1]
+		angular = vel_and_ang[1][0]
 		theta_diff = angular*T
 		theta_T = theta+theta_diff
 		posx_T = posx
 		posy_T = posy
+
+	else:
+		R = vel_and_ang[0][0]/vel_and_ang[1][0]
+		theta_T = theta + vel_and_ang[1][0]
+		posx_T = posx + R*(np.sin(theta_T) - np.sin(theta))
+		posy_T = posy + R*(np.cos(theta_T) - np.cos(theta))
+
+	params = [theta_T, posx_T, posy_T, s]
+	return params
